dd2fe6d31ab30bc7408778318b2383533b584e4c
Tonari239 <vanina103@abv.bg>
2024-01-11 23:45:56 +0200

    Enable CORS

--- AdsMicroService/Program.cs
+++ AdsMicroService/Program.cs
@@ -7,10 +7,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IRepository<Ad>, AdsRepository>();
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options =>
+    {
+        options.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
+app.UseCors("AllowOrigin");
 // Configure the HTTP request pipeline.
+
+
 
 app.UseAuthorization();
 
