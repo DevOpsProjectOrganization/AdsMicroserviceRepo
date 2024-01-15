84ed7ea7a2718b1addfdca5ceb49758f147f8cdb
Tonari239 <vanina103@abv.bg>
2024-01-16 00:36:08 +0200

    Fix faulty URL

--- AdsMicroService/Service/AdsRepository.cs
+++ AdsMicroService/Service/AdsRepository.cs
@@ -37,37 +37,43 @@ namespace AdsMicroService.Service
                     {
                         Id=2,
                         Name="Burger",
-                        Description="Eating real food is cringe. Enjoy our mystery burger. What's in it? Meat? Veggies? We don't know + who cares."
+                        Description="Eating real food is cringe. Enjoy our mystery burger. What's in it? Meat? Veggies? We don't know + who cares.",
+                        Image="https://i.imgur.com/S7lojZi.jpg"
                     },
                     new Ad
                     {
                         Id=3,
-                        Name="Doomscroller",
-                        Description="Living in the moment? Focusing on tasks? Why? Our new phone extensions automatically adds subway surfers, reddit voiceover and Minecraft playthrough to everything you do, always with you"
+                        Name="Attention-destroyer",
+                        Description="Living in the moment? Focusing on tasks? Why? Our new phone extensions automatically adds subway surfers, reddit voiceover and Minecraft playthrough to everything you do, always with you",
+                        Image="https://i.imgur.com/f6k86Xk.png"
                     },
                      new Ad
                     {
                         Id=4,
                         Name="Perfume",
-                        Description="Tired of people sitting next to you on the bus? Worry not. Our newest fragrance, Stimky, guarantees nobody will consciously approach you within a 0.5 mile radius! Get some Stimky, but not for drinky!"
+                        Description="Tired of people sitting next to you on the bus? Worry not. Our newest fragrance, Stimky, guarantees nobody will consciously approach you within a 0.5 mile radius! Get some Stimky, but not for drinky!",
+                        Image="https://newsroom.unsw.edu.au/sites/default/files/styles/full_width__2x/public/thumbnails/image/unpleasant_odours_shutterstock_1020325234_copy_1.jpg?itok=TojWU0vt"
                     },
                       new Ad
                     {
                         Id=5,
                         Name="Tyres",
-                        Description="Here for a good time, not a long one? Say no more! With our zero-friction tyres, one of the two will surely happen."
+                        Description="Here for a good time, not a long one? Say no more! With our zero-friction tyres, one of the two will surely happen.",
+                        Image="https://qph.cf2.quoracdn.net/main-qimg-b1f84ef60685649b74b8d19b6f66b16e-lq"
                     },
                         new Ad
                     {
                         Id=6,
                         Name="Watch",
-                        Description="Join the minimalistic trend with this simple watch - no dials, no indices, just swag."
+                        Description="Join the minimalistic trend with this simple watch - no dials, no indices, just swag.",
+                        Image="https://i.imgur.com/AlsGTfS.png"
                     },
                         new Ad
                     {
                         Id=7,
                         Name="Sunglasses",
-                        Description="The sun is blinding you and people are staring? No. With these blackout sunglasses, you will not only avoid eye-contact, but you will also appear dark and mysterous, because you won't be able to see anything. Get them now!"
+                        Description="The sun is blinding you and people are staring? No. With these blackout sunglasses, you will not only avoid eye-contact, but you will also appear dark and mysterous, because you won't be able to see anything. Get them now!",
+                        Image="https://goodr.com/cdn/shop/files/5_OPERATION_BLACKOUT_Featured_788c83e7-18bb-4fe3-9fe9-2f56e13e3122.jpg?crop=center&pad_color=ffffff&v=1689751191&width=1600"
                     }
                 };
         }
